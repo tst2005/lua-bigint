cc5821d2a1fbb3714173e3f78d872f568084dff4
TsT <tst2005@gmail.com>
2017-05-10 00:32:00 +0200

    safe add

--- bigint.lua
+++ bigint.lua
@@ -7,25 +7,26 @@ function bigint:init(v)
 	self.base = 10
 	self.size = 3
 	if v then
-		self.v = self:fromstring(v, 10)
-		self.strv = v
+		self:fromstring(v, 10)
+		--self.strv = v
 	end
 end
 
 function bigint:fromstring(v, base)
 	if base == 10 then
-		return self:b10fromstring(v)
+		self.v = self:_base10_fromstring(v)
+		return self.v
 	end
 	error("invalid base")
 end
 
-function bigint:b10fromstring(v)
+function bigint:_base10_fromstring(v)
 	assert(type(v)=="string")
 	assert(not v:find("^[0-9+]$"), "invalid format must be strickly [0-9]+")
 	local r = {}
 	local size = self.size
 	while #v > 0 do
-		local x = v:sub(-size, -1)
+		local x = tonumber(v:sub(-size, -1))
 		v=v:sub(1,-1-size)
 		r[#r+1] = x
 	end
@@ -44,15 +45,50 @@ end
 function bigint:tostring()
 	assert(self.v)
 	local r = {}
+	local max = #self.v
 	for _i,v in ipairs(self.v) do
 		local x = ("%.0f"):format(v)
-		if _i ~= 1 then
+		if _i ~= max then
 			x = (("0"):rep(self.size-#x))..x
 		end
 		r[#r+1] = x
 	end
-	return table.concat(table_reverse(r), "")
+	return table.concat(table_reverse(r), self.sep or "")
 end
+
+function bigint:add(mores)
+--	assert(type(n)=="number")
+	--assert(math.type(n)=="integer")
+--	assert(n>=0)
+	local max = (self.base ^ self.size)
+	local floor = math.floor
+	local abs = math.abs
+	local min = math.min
+	local function safeadd(cur, incr)
+		return
+			--abs( max - incr%max - cur) - max,
+			(max-cur >= incr%max) and cur+incr%max or ((incr%max) - (max - cur)), -- over: ce qui depasse]]
+			floor( cur / max + (incr)%max / max ) -- rest: multiple de max
+	end
+	local more = 0 --mores[1] --%max
+	local r = {}
+	for i, v in ipairs(self.v) do
+		local new = v
+		more = (more or 0) + ( mores[i] or 0)%max
+		if more and more ~= 0 then
+			local lastmore = more
+			new, more = safeadd(v, more)
+			print(i, "safeadd:", v, lastmore, "=>", new, more, "max", max)
+		end
+		r[i] = new
+	end
+	if more then
+		r[#r+1] = more
+	end
+	self.v = r
+	return self
+end
+
 local M = {}
 setmetatable(M, {__call = function(_, ...) return instance(bigint, ...) end})
 return M
--- test.bigint.lua
+++ test.bigint.lua
@@ -1,14 +1,71 @@
 local bigint = require "bigint"
 
 local x
+local src
+
+--print(require"tprint"( (bigint("1234567890").v))) -- {890,567,234,1,}
+
+--[[
 
 x = bigint("123456789012345678901234567890")
 assert(type(x) == "table")
 assert(x:tostring() == "123456789012345678901234567890")
 
 x = bigint( ("123456789012345678901234567890"):rep(5) )
-print(x:tostring())
-print("10^"..(#x.v * x.size)) -- 10^150
+assert(x:tostring()) -- 123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+assert("10^"..(#x.v * x.size)) -- 10^150
 
--- 123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
--- 10^150
+src = ("9"):rep(300)
+x = bigint()
+x.size = 8
+x:fromstring(src, 10)
+assert( (#x.v -1) * x.size + #tostring(x.v[#x.v]) == 300)
+
+src = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234560000"
+x = bigint()
+--x.sep = " " -- allow to print each group separated by space
+x.size = 8
+x:fromstring(src, 10)
+
+]]--
+
+src = "123456789012345678901234567890123456789012345678901234"
+src = "99991234"
+local a = "99991234"
+
+local tprint = require"tprint"
+tprint.ishort = false
+
+local x0 = bigint()
+x0.size = 4
+x0.sep = " "
+x0:fromstring(src, 10)
+
+local a0 = bigint()
+a0.size = 4
+a0.sep = " "
+a0:fromstring(a, 10)
+print(tprint( a0.v))
+
+x = bigint()
+x.size = 4
+x.sep = " "
+x:fromstring(src, 10)
+
+print(tprint( x.v))
+
+
+print("add", a)
+
+x:add(a0.v)
+
+print(src)
+print(x0:tostring())
+print(x:tostring())
+
+print(tprint( x0.v))
+print(tprint( x.v))
+
+--print(#x.v, x.size, #tostring(x.v[#x.v]))
+print("estimate: ~10^"..(#x.v -1) * x.size + #tostring(x.v[#x.v]))
+
